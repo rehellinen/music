77a6448416ba9bc19ca82a4c6541743abf3b290e
rehellinen <rehellinen@foxmail.com>
2019-10-21 09:31:49 +0800

    set font

--- src/App.vue
+++ src/App.vue
@@ -15,9 +15,21 @@ export default {
 
 <style lang="sass" rel="stylesheet/sass">
   @import "~assets/base"
+  @font-face
+    font-family: 'lineto'
+    src: url(./assets/lineto-brown-regular.ttf) format("truetype")
   .wrapper
     width: 100%
     min-height: 100vh
-    background-color: $purple
+    background-color: $blue
+    p
+      color: $base-font-color
+      font-family: 'lineto', sans-serif
+    span
+      color: $base-font-color
+      font-family: 'lineto', sans-serif
+    label
+      color: $base-font-color
+      font-family: 'lineto', sans-serif
   .de
 </style>
