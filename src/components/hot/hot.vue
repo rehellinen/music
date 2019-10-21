3568145cf63e1f439f9ff241fc307a23b27d9011
rehellinen <rehellinen@foxmail.com>
2019-10-21 21:10:36 +0800

    hot page

---
+++ src/components/hot/hot.vue
@@ -0,0 +1,12 @@
+<template lang="pug">
+  div
+    p hot111111111111111111
+</template>
+
+<script>
+export default {}
+</script>
+
+<style lang="sass" scoped>
+
+</style>
--- src/utils/router.js
+++ src/utils/router.js
@@ -1,11 +1,13 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Banner from '../components/banner/banner'
+import Hot from '../components/hot/hot'
 
 Vue.use(Router)
 
 const routes = [
-  { path: '/', component: Banner }
+  { path: '/', component: Banner },
+  { path: '/hot', component: Hot }
 ]
 
 export const router = new Router({
